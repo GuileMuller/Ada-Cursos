cb3bf44d3278d49d7cbab2a93bb52ceb94859ee4
GuileMuller <Siquelimuller@gmail.com>
2023-11-14 14:09:09 -0300

    Enviando pastas

---
+++ Santander-Coders-2023/IntelliJ/Introducao-ao-Java/Operadores-Booleanos/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean fimDeSemana = true;
+        boolean fazendoSol = true;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+
+        System.out.println(vamosAPraia);
+    }
+}
